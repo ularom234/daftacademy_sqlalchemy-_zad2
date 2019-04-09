490f1b92d9957f5fa1a3cbf6df5947da98c78216
Urszula Romaniuk <49236837+ularom234@users.noreply.github.com>
2019-04-09 16:41:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 import models
 from models import Base
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['postgres://yraipitobmtkah:a1f09eee6ca129801364fd95fa9c3f5d468714ab6c9f1f3207578a1b00882a91@ec2-79-125-2-142.eu-west-1.compute.amazonaws.com:5432/d5rksg9bnf4fdi
+']
 
 # engine = create_engine("postgresql://postgres:postgres@localhost:5432/chinook")
 engine = create_engine(DATABASE_URL)
@@ -31,5 +32,23 @@ def longest_tracks():
     tracks = db_session.query(models.Track).order_by(models.Track.milliseconds).limit(10).all()
     return jsonify(dict(tracks))
 
+                          
+@app.route("/artists")
+def artists():
+    if request.method == "GET":
+        return get_artists()
+    abort(405)
+
+
+def get_artists():
+    artists = db_session.query(models.Artist).order_by(models.Artist.name)
+    return "<br>".join(
+        f"{idx}. {artist.name}" for idx, artist in enumerate(artists)
+    )
+
+
+
+                          
+                          
 if __name__ == "__main__":
     app.run(debug=False)
